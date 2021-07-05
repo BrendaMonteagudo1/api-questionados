2b29cdfe62ccb2068e7d8ac5181bcb19fddfabfe
ShadowDrone <shadowdrone@hotmail.com>
2021-07-05 19:38:19 -0300

    Se agregan json Ignore. nombre de columna para es_correcta y rutas de categorias.
    Co-authored-by: flordife <flordife@users.noreply.github.com>
    Co-authored-by: AgosBC <AgosBC@users.noreply.github.com>
    Co-authored-by: aguspecuch <aguspecuch@users.noreply.github.com>
    Co-authored-by: magira01 <magira01@users.noreply.github.com>
    Co-authored-by: byteMe907 <byteMe907@users.noreply.github.com>

--- src/main/java/ar/com/ada/api/questionados/entities/Respuesta.java
+++ src/main/java/ar/com/ada/api/questionados/entities/Respuesta.java
@@ -2,6 +2,8 @@ package ar.com.ada.api.questionados.entities;
 
 import javax.persistence.*;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name="respuesta")
 public class Respuesta {
@@ -13,10 +15,12 @@ public class Respuesta {
 
     private String texto;
 
+    @Column(name="es_correcta")
     private boolean esCorrecta;
 
     @ManyToOne
     @JoinColumn(name="pregunta_id",referencedColumnName = "pregunta_id")
+    @JsonIgnore
     private Pregunta pregunta;
 
 
